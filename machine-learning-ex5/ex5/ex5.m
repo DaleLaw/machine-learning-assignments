d656146ddacef915976fa485116bf220190d9c9c
Dale Law <developerdalelaw@gmail.com>
2017-07-06 17:06:41 +0800

    Done optional exercise 3.4 in ex5

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -218,3 +218,26 @@ end
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
+
+
+
+
+%% =========== Optional (ungraded) exercise: Computing test set error =============
+
+[lambda_vec, error_train, error_test] = ...
+    validationCurve(X_poly, y, X_poly_test, ytest);
+
+close all;
+plot(lambda_vec, error_train, lambda_vec, error_test);
+legend('Train', 'Test');
+xlabel('lambda');
+ylabel('Error');
+
+fprintf('lambda\t\tTrain Error\tTest Error\n');
+for i = 1:length(lambda_vec)
+	fprintf(' %f\t%f\t%f\n', ...
+            lambda_vec(i), error_train(i), error_test(i));
+end
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
